1155b2a13ef2fdf4ade1ca2f43f8b774e5db4eb9
dfan <dfan@lyft.com>
2019-08-28 10:01:53 -0700

    question 11

---
+++ c_hardway/checkin.sh
@@ -0,0 +1,5 @@
+
+git add -A;
+git commit -m "question $1"
+git push origin master
+
---
+++ c_hardway/draft.sh
@@ -0,0 +1,7 @@
+mkdir question_$1
+
+cd question_$1
+
+touch question_$1.c
+
+printf "#include<stdio.h>\n\nint main(int argc, char* argv[]){\n\n\n}" >> question_$1.c
